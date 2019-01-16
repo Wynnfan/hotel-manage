6a755f28a64a7afd7e6e72930ea4fca3570bb428
Wynnfan <Wynnfan1997@gmail.com>
2019-01-17 02:00:49 +0800

    update

--- config/index.js
+++ config/index.js
@@ -11,13 +11,13 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       'api': {
-        target: 'http://localhost:3000',
+        target: 'http://47.102.118.234:3000',
         changeOrigin: true
       }
     },
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 9528, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
